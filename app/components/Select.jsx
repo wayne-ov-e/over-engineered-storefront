8c1e92d69cc6a6a6d1e71d4e5baf9c434c0fe95d
wayne-ov-e <140125789+wayne-ov-e@users.noreply.github.com>
2024-07-28 18:27:42 -0700

    fix issues

--- app/components/Select.jsx
+++ app/components/Select.jsx
@@ -1,18 +1,35 @@
 import styles from '~/styles/components/Select.module.css';
-import { Link } from '@remix-run/react';
+import { Link, useNavigate } from '@remix-run/react';
+import { useState } from 'react';
 
 function Select({ option }) {
+    const navigate = useNavigate();
+    let defaultValue = option.values.find(({ isActive }) => isActive)?.to || option.values[0].to;
+
+    const handleChange = (event) => {
+        const selectedRoute = event.target.value;
+        if (selectedRoute) {
+            navigate(selectedRoute);
+        }
+    };
+
     return (
         <div className='relative'>
-            <select className={`${styles.select} !outline-none w-full`} name={option.name} id={option.name} defaultValue={option.values[0].value}>
+            <select
+                className={`${styles.select} !outline-none w-full`}
+                name={option.name}
+                id={option.name}
+                defaultValue={defaultValue}
+                onChange={handleChange}
+            >
                 {option.values.map(({ value, isAvailable, to, isActive }) => {
                     return (
-                        <option key={value} value={value} disabled={!isAvailable}>{value}</option>
+                        <option key={value} value={to} disabled={!isAvailable}>{value}</option>
                     );
                 })}
             </select>
             <svg className={`${styles.select_caret}`} aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 10 6">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z" fill="currentColor">
+                <path fillRule="evenodd" clipRule="evenodd" d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z" fill="currentColor">
                 </path>
             </svg>
         </div>
